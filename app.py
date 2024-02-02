053c00c66fd67041e7b27e48cc6a55cbfe8e1996
Samer Kharabish <s.kharabish@truma.com>
2024-02-02 15:58:07 +0100

    Added an entry point for the application

---
+++ app.py
@@ -0,0 +1,15 @@
+"""Entry point of the application."""
+
+import customtkinter as ctk
+
+
+def main():
+    """
+    Sets up the main window and event loop.
+    """
+    root = ctk.CTk()
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
