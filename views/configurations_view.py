24f85a02664550f5a4d1fcd84814496af752479e
Samer Kharabish <s.kharabish@truma.com>
2024-02-02 18:07:57 +0100

    Added title config

---
+++ views/configurations_view.py
@@ -0,0 +1,14 @@
+"""Centralized configuration file for the visual representation of the application."""
+
+
+class Config:
+    """
+    Contains classes that define the configurations of the visual representation of the application.
+    """
+
+    class Titles:
+        """
+        Titles
+        """
+
+        MAIN_WINDOW_TITLE = "CSVision"
