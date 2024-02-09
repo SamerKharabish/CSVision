c88fbc53ba23185b175ddef105a90545b2cba1c4
Samer Kharabish <158495900+SamerKharabish@users.noreply.github.com>
2024-02-09 15:43:31 +0100

    Refactored the entry of the application (#10)
    Co-authored-by: Samer Kharabish <s.kharabish@truma.com>

--- app.py
+++ app.py
@@ -6,37 +6,55 @@ from views.configurations_view import Config
 from controllers.main_controller import MainController
 
 
+class AppView(ctk.CTk):
+    """
+    Layout of the main application.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.title(Config.Titles.APP_WINDOW_TITLE)
+        self.iconbitmap(Config.Images.MAIN_WINDOW_ICON)
+        self.geometry(
+            f"{Config.Dimensions.APP_WINDOW_WIDTH}x{Config.Dimensions.APP_WINDOW_HEIGHT}+{int(self.winfo_screenwidth() / 2 - Config.Dimensions.APP_WINDOW_WIDTH / 2)}+{int(self.winfo_screenheight() / 2 - Config.Dimensions.APP_WINDOW_HEIGHT / 2)}"
+        )
+        self.minsize(
+            Config.Dimensions.APP_WINDOW_MIN_WIDTH,
+            Config.Dimensions.APP_WINDOW_MIN_HEIGHT,
+        )
+
+
+class AppController:
+    """
+    Functionality of the main application.
+    """
+
+    def __init__(self, view=None) -> None:
+        self.view = view
+
+        # accessibility events
+        self.view.bind("<Escape>", self.close_application)
+        self.view.protocol("WM_DELETE_WINDOW", self.close_application)
+
+    def close_application(self, _=None):
+        """
+        Close the application.
+        """
+        self.view.destroy()
+        sys.exit(0)
+
+
 def main():
     """
     Sets up the main window and event loop.
     """
     ctk.set_appearance_mode("Dark")
-    root = ctk.CTk()
 
-    def close_application(_=None):
-        """
-        Close the application.
-        """
-        root.destroy()
-        sys.exit(0)
-
-    # security events
-    root.bind("<Escape>", close_application)
-    root.protocol("WM_DELETE_WINDOW", close_application)
-
-    root.title(Config.Titles.APP_WINDOW_TITLE)
-    root.iconbitmap(Config.Images.MAIN_WINDOW_ICON)
-    root.geometry(
-        f"{Config.Dimensions.APP_WINDOW_WIDTH}x{Config.Dimensions.APP_WINDOW_HEIGHT}+{int(root.winfo_screenwidth() / 2 - Config.Dimensions.APP_WINDOW_WIDTH / 2)}+{int(root.winfo_screenheight() / 2 - Config.Dimensions.APP_WINDOW_HEIGHT / 2)}"
-    )
-    root.minsize(
-        Config.Dimensions.APP_WINDOW_MIN_WIDTH,
-        Config.Dimensions.APP_WINDOW_MIN_HEIGHT,
-    )
-
-    MainController(root)
-
-    root.mainloop()
+    app_view = AppView()
+    app = AppController(app_view)
+    MainController(app.view)
+    app.view.mainloop()
 
 
 if __name__ == "__main__":
