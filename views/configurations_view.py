d9e73a24eb1ac5e4f360910dc21c1a69bf4318ff
Samer Kharabish <158495900+SamerKharabish@users.noreply.github.com>
2024-02-19 18:37:38 +0100

    Feature signal list layout (#13)
    * Added more documentation and type hinting
    
    * Added widgets to the signal list frame
    
    ---------
    
    Co-authored-by: Samer Kharabish <s.kharabish@truma.com>

--- views/configurations_view.py
+++ views/configurations_view.py
@@ -1,5 +1,7 @@
 """Centralized configuration file for the visual representation of the application."""
 
+from typing import Tuple
+
 
 class Config:
     """
@@ -14,7 +16,7 @@ class Config:
         CORNER_RADIUS: int = 0
         BORDER_WIDTH: int = 1
 
-    class Titles:
+    class WindowTitles:
         """
         Titles
         """
@@ -32,9 +34,13 @@ class Config:
         APP_WINDOW_MIN_HEIGHT: int = APP_WINDOW_HEIGHT - 200
 
         SIGNAL_FRAME_WIDTH: int = int(APP_WINDOW_WIDTH * 0.25)
-        SIGNAL_FRAME_MIN_WIDTH: int = 50
+        SIGNAL_FRAME_MIN_WIDTH: int = 40
 
-        STATUSBAR_FRAME_HEIGHT: int = 30
+        STATUSBAR_FRAME_HEIGHT: int = 20
+
+        FILEHANDLING_FRAME_HEIGHT: int = 80
+        SEARCHBAR_FRAME_HEIGHT: int = FILEHANDLING_FRAME_HEIGHT
+
 
     class Images:
         """
@@ -60,9 +66,31 @@ class Config:
         SIGNAL_FRAME_FILL: str = "y"
         SIGNAL_FRAME_EXPAND: bool = False
 
+        SIGNAL_FRAME_LABELS_ANCHOR: str = "w"
+        SIGNAL_FRAME_LABELS_SIDE: str = "top"
+        SIGNAL_FRAME_LABELS_FILL: str = "x"
+        SIGNAL_FRAME_LABELS_EXPAND: bool = False
+        SIGNAL_FRAME_LABELS_PADX: Tuple[int, int] = (20, 5)
+        SIGNAL_FRAME_LABELS_PADy: Tuple[int, int] = (1, 2)
+
+        SIGNAL_FRAME_SCROLLABLEFRAME_SIDE: str = "top"
+        SIGNAL_FRAME_SCROLLABLEFRAME_FILL: str = "both"
+        SIGNAL_FRAME_SCROLLABLEFRAME_EXPAND: bool = True
+        SIGNAL_FRAME_SCROLLABLEFRAME_PADX: Tuple[int, int] = (1, 1)
+        SIGNAL_FRAME_SCROLLABLEFRAME_PADy: Tuple[int, int] = (1, 1)
+
         PLOT_FRAME_SIDE: str = "right"
         PLOT_FRAME_FILL: str = "both"
         PLOT_FRAME_EXPAND: bool = True
+
+        FILEHANDLING_ROW: int = 0
+        FILEHANDLING_STICKY: str = "ew"
+        SEARCHBAR_ROW: int = 1
+        SEARCHBAR_STICKY: str = "ew"
+        SIGNALLIST_ROW: int = 2
+        SIGNALLIST_STICKY: str = "nesw"
+        CONFIG_SIGNALLIST_ROW: int = 3
+        CONFIG_SIGNALLIST_STICKY: str = "nesw"
 
     class KeyBindings:
         """
@@ -71,3 +99,21 @@ class Config:
 
         CLOSE_APPLICATION: str = "<Escape>"
         RESIZE_SIGNAL_FRAME: str = "<Control-b>"
+
+    class LabelTexts:
+        """
+        Label texts
+        """
+
+        FILEHANDLING_TEXT: str = "EXPLORER"
+        SEARCHBAR_TEXT: str = "SEARCH"
+        SIGNALLIST_TEXT: str = "SIGNALS"
+        CONFIG_SIGNALLIST_TEXT: str = "CONFIGURATIONS"
+
+    class Fonts:
+        """
+        Fonts
+        """
+
+        LABEL_TEXTS: Tuple[str, int] = ("Calibri", 14)
+
